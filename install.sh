1df2caa0ced7a096f706955e28157ad95849e30a
dinuka <dinukasal@gmail.com>
2017-11-12 13:59:23 +0000

    added -y

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 echo "installing lamp.."
 sudo apt update
-sudo apt install apache2
+sudo apt install apache2 -y
 sudo apache2ctl configtest
 
 echo "Adjust the Firewall to Allow Web Traffic"
@@ -13,10 +13,10 @@ echo "Allow incoming traffic for this profile"
 sudo ufw allow in "Apache Full"
 
 echo "installing mysql.."
-sudo apt install mysql-server
+sudo apt install mysql-server -y
 
 echo "installing php.."
-sudo apt install php libapache2-mod-php php-mcrypt php-mysql
+sudo apt install php libapache2-mod-php php-mcrypt php-mysql -y
 
 sudo systemctl status apache2
 
