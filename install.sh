98e583f4d2bef794484506e928255d8871a07085
dinuka <dinukasal@gmail.com>
2017-11-15 03:45:44 +0000

    mvn fix

--- install.sh
+++ install.sh
@@ -202,7 +202,7 @@ elif [ $input == "mvn" ] || [ $input == "maven" ] ; then
 		sudo wget http://www-eu.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
 		sudo tar -xvzf apache-maven-3.3.9-bin.tar.gz
 		sudo mv apache-maven-3.3.9 maven 
-		echo 'export M2_HOME=/opt/maven
+		sudo echo 'export M2_HOME=/opt/maven
 				export PATH=${M2_HOME}/bin:${PATH}' > /etc/profile.d/mavenenv.sh;
 		sudo chmod +x /etc/profile.d/mavenenv.sh
 		sudo source /etc/profile.d/mavenenv.sh
