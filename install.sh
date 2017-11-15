9dd0241d03ae6f3984f353cbafe273cc692b2fac
dinuka <dinukasal@gmail.com>
2017-11-15 03:44:46 +0000

    mvn fix

--- install.sh
+++ install.sh
@@ -199,7 +199,7 @@ elif [ $input == "mvn" ] || [ $input == "maven" ] ; then
 		sudo apt update -y
 		sudo apt install -y oracle-java8-installer
 		cd /opt/
-		wget http://www-eu.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
+		sudo wget http://www-eu.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
 		sudo tar -xvzf apache-maven-3.3.9-bin.tar.gz
 		sudo mv apache-maven-3.3.9 maven 
 		echo 'export M2_HOME=/opt/maven
